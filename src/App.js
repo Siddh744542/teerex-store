82c54cb12a1cfdbb6349a8ca3289af924b576081
Siddharth Tiwari <tiwarisiddharth27@gmail.com>
2023-08-11 11:49:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import DataProvider from './context/dataProvider';
+import {BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+
+//components
+import Home from "./components/home/Home";
+import Cart from './components/cart/Cart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataProvider>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/cart' element={<Cart />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+      
+    </DataProvider>
+    
   );
 }
 
---
+++ src/context/dataProvider.jsx
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react";
+
+export const DataContext = createContext(null);
+
+const DataProvider = ({children})=>{
+    const [products, setProducts] = useState({});
+    const [cart, setCart] = useState({});
+    return(
+        <DataContext.Provider value={{
+            products,
+            setProducts,
+            cart,
+            setCart
+        }}> 
+        {children}
+        </DataContext.Provider>
+    )
+}
+
+export default DataProvider;
